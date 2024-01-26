10d24393f5fc4c6fd88e3d0e6386ac70d5ee2376
How Bizarre <howbizarre@gmail.com>
2024-01-26 17:23:06 +0200

    Update devtools and styling in nuxt.config.ts and [slug].vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,51 +6,36 @@ useHead({
 
 <template>
   <div>
-    <!-- Blog List  -->
-    <ContentList
-      path="/posts"
-      fields="title,date,thumbnail"
-      :query="{
-        draft: false,
-        sort: [
-          {
-            date: -1,
-          },
-        ],
-      }"
-      v-slot="{ list }"
-    >
-      <!-- Blog Card  -->
-      <div
-        v-for="blog in list"
-        :key="blog._path"
-        class="blog-card bg-white rounded-2xl overflow-hidden mb-4"
-      >
+    <ContentList path="/posts"
+                 fields="title,date,thumbnail"
+                 :query="{ draft: false, sort: [{ date: -1 }] }"
+                 v-slot="{ list }">
+      <div v-for="blog in list"
+           :key="blog._path"
+           class="blog-card bg-white rounded-2xl overflow-hidden mb-4">
         <div class="h-[300px] relative">
-          <img
-            v-if="blog.thumbnail"
-            :src="blog.thumbnail"
-            :alt="blog.title"
-            class="absolute w-full h-full object-cover"
-          />
+          <img v-if="blog.thumbnail"
+               :src="blog.thumbnail"
+               :alt="blog.title"
+               class="absolute w-full h-full object-cover" />
         </div>
 
         <div class="blog-card--meta my-4 ml-4">
           <h3 class="text-2xl font-bold">
             <NuxtLink :to="blog.slug">{{ blog.title }}</NuxtLink>
           </h3>
-          <div class="text-sm text-gray-500 mt-px block">{{ blog.date }}</div>
-          <div v-if="blog.tags" class="mt-2 text-xs">
-            <span v-for="tag in blog.tags" class="p-1 rounded bg-gray-100 mr-2">
-              {{ tag }}</span
-            >
-          </div>
+
+          <div class="text-sm text-gray-500 mt-px block">{{ (`${blog.date}`).split("T")[0] }}</div>
+          
+          <template v-if="blog.tags">
+            <div class="mt-2 text-xs">
+              <template v-for="tag in blog.tags">
+                <span class="px-1 pb-1 rounded bg-gray-100 border border-gray-400/25 mr-2 shadow-sm capitalize cursor-pointer">{{ tag }}</span>
+              </template>
+            </div>
+          </template>
         </div>
       </div>
-      <!-- ./ Blog Card  -->
     </ContentList>
-    <!-- ./ Blog List  -->
   </div>
 </template>
-
-<style></style>
