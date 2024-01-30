d0ececc804b809bdf3fbe050c2066711773e3fab
How Bizarre <howbizarre@gmail.com>
2024-01-30 16:41:52 +0200

    Refactor app.vue and tailwind.css

---
+++ components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="mt-8 mb-4 flex justify-between items-end">
+    <div>
+      <h1 class="text-2xl font-semibold">
+        <NuxtLink to="/">Nuxt Blog</NuxtLink>
+      </h1>
+      <p class="text-gray-500">Simple blog powered by Nuxt Content</p>
+    </div>
+
+    <ColorMode />
+  </header>
+</template>
